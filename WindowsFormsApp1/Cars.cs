1b33ce4417aba9e960b4af3e096650d2319f9696
Akbari <Akbari_h@outlook.com>
2019-06-10 08:15:01 +0430

    Add Class Car.cs

---
+++ WindowsFormsApp1/Cars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public String NameCar { get; set; }
+    }
+}
